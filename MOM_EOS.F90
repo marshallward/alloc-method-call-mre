9709cf74143d58f582e7bede0d91e62412bc1b1f
Marshall Ward <marshall.ward@noaa.gov>
2024-10-04 12:26:31 -0400

    Arguments removed
    Just about everything is gone at this point.  It's just an empty
    subroutine.  Still gets the error.  Just about reached the MRE in this
    case.

--- MOM_EOS.F90
+++ MOM_EOS.F90
@@ -10,19 +10,9 @@ contains
 
 subroutine calculate_density_1d(T, S, pressure, rho, EOS)
   !$acc routine
-  real, dimension(:),    intent(in)    :: T        !< Potential temperature referenced to the surface [C ~> degC]
-  real, dimension(:),    intent(in)    :: S        !< Salinity [S ~> ppt]
-  real, dimension(:),    intent(in)    :: pressure !< Pressure [R L2 T-2 ~> Pa]
-  real, dimension(:),    intent(inout) :: rho      !< Density (in-situ if pressure is local) [R ~> kg m-3]
-  type(EOS_type),        intent(in)    :: EOS      !< Equation of state structure
+  type(EOS_type), intent(in) :: EOS
 
-  ! Local variables
-  real :: rho_scale ! A factor to convert density from kg m-3 to the desired units [R m3 kg-1 ~> 1]
-  real, dimension(size(rho)) :: pres  ! Pressure converted to [Pa]
-  real, dimension(size(rho)) :: Ta    ! Temperature converted to [degC]
-  real, dimension(size(rho)) :: Sa    ! Salinity converted to [ppt]
-
-  call EOS%type%calculate_density_array(T, S, pressure, rho)
+  call EOS%type%calculate_density_array()
 end subroutine calculate_density_1d
 
 end module MOM_EOS
--- MOM_EOS_base_type.F90
+++ MOM_EOS_base_type.F90
@@ -2,32 +2,13 @@ module MOM_EOS_base_type
 
 type, abstract :: EOS_base
   contains
-  procedure(i_density_elem), deferred :: density_elem
   procedure :: calculate_density_array => a_calculate_density_array
 end type EOS_base
 
-interface
-  real elemental function i_density_elem(this, T, S, pressure)
-    import :: EOS_base
-    class(EOS_base), intent(in) :: this     !< This EOS
-    real,            intent(in) :: T        !< Potential temperature relative to the surface [degC]
-    real,            intent(in) :: S        !< Salinity [PSU]
-    real,            intent(in) :: pressure !< Pressure [Pa]
-  end function i_density_elem
-end interface
-
 contains
 
-!> Calculate the in-situ density for 1D arraya inputs and outputs.
-!subroutine a_calculate_density_array(this, T, S, pressure, rho, start, npts)
-subroutine a_calculate_density_array(this, T, S, pressure, rho)
-  class(EOS_base), intent(in) :: this     !< This EOS
-  real, dimension(:), intent(in)  :: T        !< Potential temperature relative to the surface [degC]
-  real, dimension(:), intent(in)  :: S        !< Salinity [PSU]
-  real, dimension(:), intent(in)  :: pressure !< Pressure [Pa]
-  real, dimension(:), intent(out) :: rho      !< In situ density [kg m-3]
-
-  rho(:) = this%density_elem(T(:), S(:), pressure(:))
+subroutine a_calculate_density_array(this)
+  class(EOS_base), intent(in) :: this
 end subroutine a_calculate_density_array
 
 end module MOM_EOS_base_type
