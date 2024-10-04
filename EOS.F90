47814d3fe60dc5605ee966419e244f50fde3dfe9
Marshall Ward <marshall.ward@noaa.gov>
2024-10-04 15:20:43 -0400

    Rename "type" to "etype"
    Just to avoid any potential parsing issue.

--- EOS.F90
+++ EOS.F90
@@ -4,7 +4,7 @@ use EOS_base_mod, only : EOS_base
 implicit none
 
 type :: EOS_type
-  class(EOS_base), allocatable :: type
+  class(EOS_base), allocatable :: etype
 end type
 
 contains
@@ -13,7 +13,7 @@ subroutine calculate_density_1d(EOS)
   !$acc routine
   type(EOS_type), intent(in) :: EOS
 
-  call EOS%type%calc_density_array()
+  call EOS%etype%calc_density_array()
 end subroutine calculate_density_1d
 
 end module EOS
